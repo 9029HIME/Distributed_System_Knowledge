73992e8befe1c58ab4b2ebee37cb72c63930d0dd
koujyungenn <513137767@qq.com>
2022-05-23 21:10:36 +0800

    Sentinel暂时完结，准备看分布式事务

--- user-service/src/main/java/cn/itcast/user/service/UserService.java
+++ user-service/src/main/java/cn/itcast/user/service/UserService.java
@@ -12,6 +12,11 @@ public class UserService {
     private UserMapper userMapper;
 
     public User queryById(Long id) {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return userMapper.findById(id);
     }
 }
